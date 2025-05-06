98778a24e765634796d2775ce6aeae62a886db8d
ElisaVicomanni <elisa.vicomanni@gmail.com>
2025-05-06 16:26:59 +0200

    merge effettuato

--- backend/api/structs/employee_struct.go
+++ backend/api/structs/employee_struct.go
@@ -1,20 +1,6 @@
 package structs
 
 type Employee struct {
-<<<<<<< HEAD
-	ID         int     `json:"id"`
-	Name       string  `json:"name"`
-	Surname    string  `json:"surname"`
-	Email      string  `json:"email"`
-	Phone      string  `json:"phone"`
-	Address    string  `json:"address"`
-	BirthDate  string  `json:"birth_date"`
-	HireDate   string  `json:"hire_date"`
-	Salary     float64 `json:"salary"`
-	Department string  `json:"department"`
-	Position   string  `json:"position"`
-	Project    int     `json:"project"`
-=======
 	ID          int     `json:"id"`
 	NameSurname string  `json:"name_surname"`
 	Email       string  `json:"email"`
@@ -26,5 +12,4 @@ type Employee struct {
 	Department  string  `json:"department"`
 	Position    string  `json:"position"`
 	Project     int     `json:"project"`
->>>>>>> origin/master
 }
